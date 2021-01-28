d8f1127f7393a4f26b22f6e794e7433ced74eb70
David Ospina <jdospin@hotmail.com>
2021-01-27 22:25:39 -0500

    Add Prettier and ESlint configuration to the project

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan('dev')); // this middleware is used to log all the requestests before processing them
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // this middleware is used to log all the requestests before processing them
+}
 app.use(express.json()); // this middleware is used to parse json data sent in the request body
 // app.use(express.static(`${__dirname}/public`)); // We can serve static files from the server with express.static
 
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,63 +1,76 @@
 const fs = require('fs');
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price'
+      message: 'Missing name or price',
     });
   }
   next();
 };
 
 exports.checkID = (req, res, next, val) => {
-  const id = parseInt(val);
+  const id = parseInt(val, 10);
+
   if (id > tours.length) {
-    return res.status(404).json({ // Not found
+    return res.status(404).json({
+      // Not found
       status: 'fail',
-      message: 'Invalid ID'
+      message: 'Invalid ID',
     });
   }
   next();
 };
 
 exports.getAllTours = (req, res) => {
-  res.status(200).json({ // OK
+  res.status(200).json({
+    // OK
     status: 'success',
     results: tours.length,
     data: {
-      tours
-    }
+      tours,
+    },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = parseInt(req.params.id);
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({ // OK
+  const id = parseInt(req.params.id, 10);
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    // OK
     status: 'success',
     data: {
-      tour
-    }
+      tour,
+    },
   });
 };
 
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({
-    id: newId
-  }, req.body);
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
 
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({ // created
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    })
-  });
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        // created
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 };
 
 exports.updateTour = (req, res) => {
@@ -65,15 +78,16 @@ exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour here...>'
-    }
+      tour: '<Updated tour here...>',
+    },
   });
 };
 
 exports.deleteTour = (req, res) => {
   // const id = parseInt(val); // will need it in the future
-  res.status(204).json({ // no content
+  res.status(204).json({
+    // no content
     status: 'success',
-    data: null
+    data: null,
   });
 };
