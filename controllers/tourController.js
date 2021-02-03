31979072323f4ba475977745e21235d55b33350e
David Ospina <jdospin@hotmail.com>
2021-02-03 15:07:50 -0500

    Add field sorting and field filtering to the Tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,22 +21,15 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = {
       ...req.query,
     };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advanced filtering
+    // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
-
-    // We add a '$' before each conditional expression (gte, gt, lte, lt) in order to use them as a filter in mongoose's find method
-    // ex: { name: 'Juan', age: { $lt: 30 }}
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    const query = Tour.find(JSON.parse(queryStr));
-
     // Can also be done like this
     // const query = Tour.find()
     //   .where('duration')
@@ -44,7 +37,42 @@ exports.getAllTours = async (req, res) => {
     //   .where('difficulty')
     //   .equals('easy');
 
-    // 3) EXECUTE QUERY
+    // We add a '$' before each conditional expression (gte, gt, lte, lt) in order to use them as a filter in mongoose's find method
+    // Example of a url with filter params: 127.0.0.1:3000/api/v1/tours?duration[gte]=5&difficulty=easy&price[lt]=1500
+    // queryStr = { duration: { $gte: 5 }, difficulty: 'easy', price: { $lt: 1500 }}
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORTING
+
+    // We can provide multiple params to sort the results: price, ratingsAgerage
+    // Example of a url with sort params: 127.0.0.1:3000/api/v1/tours?sort=-price,ratingsAverage
+    // The '-' in '-price' means sort descending
+    /* From mongoose docs:
+      // sort by "field" ascending and "test" descending
+      query.sort({ field: 'asc', test: -1 });
+
+      // equivalent
+      query.sort('field -test');
+    */
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy); // we can sort the data using the mongoose Model's sort method and sending to it the sort criteria in the query param
+    } else {
+      query = query.sort('-createdAt'); // By default, we will sort by creation date descending (most recent first)
+    }
+
+    // 3) FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      // exclude default document version field from the result
+      query = query.select('-__v'); // Here we select all fields except '__v' which is used internally by mongoose. More on this: https://stackoverflow.com/questions/12495891/what-is-the-v-field-in-mongoose
+    }
+
+    // EXECUTE QUERY
     const tours = await query;
 
     // SEND RESPONSE
