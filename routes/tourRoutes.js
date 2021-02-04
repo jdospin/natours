52c8b344ea5a3d312076fab0273b292d456583e3
David Ospina <jdospin@hotmail.com>
2021-02-03 21:48:02 -0500

    Add new Tour route for top 5 cheapest tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,11 +5,13 @@ const {
   deleteTour,
   getAllTours,
   getTour,
+  aliasTopTours,
 } = require('../controllers/tourController');
 
 const tourRouter = express.Router(); // Creation of a new route. It's like having an app inside another app...
 // tourRouter.param('id', checkID); // this middleware was used to check the id param before calling the corresponding function in the controller
 
+tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours); // calling the getAllTours after calling a new middleware to add the required params to the query string to get only the top 5 cheapest tours
 tourRouter.route('/').get(getAllTours).post(createTour);
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
