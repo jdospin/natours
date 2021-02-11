b924d2a2ab1cca6e02ceeeebfb7ab779cbcec791
David Ospina <jdospin@hotmail.com>
2021-02-11 15:33:29 -0500

    Add global middleware to handle 404 requests

--- app.js
+++ app.js
@@ -15,5 +15,11 @@ app.use(express.json()); // this middleware is used to parse json data sent in t
 // 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
