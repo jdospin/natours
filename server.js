551fca62f80d91f7bba79df3dfc3604764aece17
David Ospina <jdospin@hotmail.com>
2021-02-24 19:28:56 -0500

    Add global error handlers for the server application

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught exception. Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env',
 });
@@ -21,6 +27,14 @@ mongoose
   .then(() => console.log('Connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App runing on port ${port} ...`); // eslint-disable-line
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled rejection. Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
