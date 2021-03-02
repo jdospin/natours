79ef920ad090a198ebe72a6a5eed483aabd9ab09
David Ospina <jdospin@hotmail.com>
2021-03-01 21:18:11 -0500

    Add route restrictions based on users roles

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const {
-  createTour,
-  updateTour,
-  deleteTour,
-  getAllTours,
-  getTour,
-  aliasTopTours,
-  getTourStats,
-  getMonthlyPlan,
-} = require('../controllers/tourController');
-const { protect } = require('../controllers/authController');
+const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 const tourRouter = express.Router(); // Creation of a new route. It's like having an app inside another app...
 // tourRouter.param('id', checkID); // this middleware was used to check the id param before calling the corresponding function in the controller
 
-tourRouter.route('/top-5-cheap').get(aliasTopTours, getAllTours); // calling the getAllTours after calling a new middleware to add the required params to the query string to get only the top 5 cheapest tours
-tourRouter.route('/tour-stats').get(getTourStats);
-tourRouter.route('/monthly-plan/:year').get(getMonthlyPlan);
-tourRouter.route('/').get(protect, getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+tourRouter
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours); // calling the getAllTours after calling a new middleware to add the required params to the query string to get only the top 5 cheapest tours
+tourRouter.route('/tour-stats').get(tourController.getTourStats);
+tourRouter.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+tourRouter
+  .route('/')
+  .get(authController.protect, tourController.getAllTours)
+  .post(tourController.createTour);
+tourRouter
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = tourRouter;
