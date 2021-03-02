70f8615169779f875f60e623fdb4dcc0c132ef38
David Ospina <jdospin@hotmail.com>
2021-03-02 13:58:06 -0500

    Add reset password capabilities with generated token sent by email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
-const { appendFile } = require('fs');
+const crypto = require('crypto');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -130,8 +130,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
       message: 'Token sent to email!',
     });
   } catch (err) {
+    // delete temporary token and expiry date
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
+
+    // disable validations to allow saving last modified fields
     user.save({ validateBeforeSave: false });
 
     return next(
@@ -144,4 +147,32 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  // find the user that has this hashed token AND is not expired yet
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
