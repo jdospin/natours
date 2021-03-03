089c2901c40073dd30e15d997acfd85916496cf1
David Ospina <jdospin@hotmail.com>
2021-03-02 23:53:38 -0500

    Implement rate limiter to protect api from attacks

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,10 +8,19 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // this middleware is used to log all the requestests before processing them
 }
+
+// max 100 requests from the same IP address in 1 hour
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json()); // this middleware is used to parse json data sent in the request body
 // app.use(express.static(`${__dirname}/public`)); // We can serve static files from the server with express.static
 
