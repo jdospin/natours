79dc159ba43ec08b6b1978eafd9068807f8106ff
David Ospina <jdospin@hotmail.com>
2021-03-04 10:54:06 -0500

    Add review controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,43 @@
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+// 1 get a review
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new AppError('No review was found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
+    },
+  });
+});
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 200,
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+// 2 creating a review
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
