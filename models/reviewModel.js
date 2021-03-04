f500526b4db4cee52f5309516ab198f1d9e65db7
David Ospina <jdospin@hotmail.com>
2021-03-03 22:34:23 -0500

    Create review model

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'A review must have a rating'],
+      min: [1.0, 'Rating must be between 1.0 and 5.0'],
+      max: [5.0, 'Rating must be between 1.0 and 5.0'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must reference a Tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
