4107126ec5ff0b301c28a07d5e16701d3971de86
David Ospina <jdospin@hotmail.com>
2021-03-11 19:53:42 -0500

    Configure project to include pug files. Create base pug file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const path = require('path');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -13,7 +14,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+// We can serve static files from the server with express.static
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set Security HTTP headers
 app.use(helmet());
 
@@ -54,10 +62,13 @@ app.use(
   })
 );
 
-// We can serve static files from the server with express.static
-app.use(express.static(`${__dirname}/public`));
-
 // 2) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Juan David',
+  });
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
