6c62a71b2e38a2831a8162f2e5ad1c0b9f94a92b
David Ospina <jdospin@hotmail.com>
2021-03-15 18:25:42 -0400

    Implement tour detail page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
-const catcAsync = require('../utils/catchAsync');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catcAsync(async (req, res, next) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) get tour data from collection
   const tours = await Tour.find();
 
@@ -14,8 +14,18 @@ exports.getOverview = catcAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res, next) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) get the data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review rating user',
+  });
+
+  // 2) Build Template
+  // 3) Render template using data from 1)
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
