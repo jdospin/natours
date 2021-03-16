fadce8da1339e75cab2f477fb5c04189e90e71c2
David Ospina <jdospin@hotmail.com>
2021-03-16 15:11:28 -0400

    Add map and pins to tour page using mapbox

---
+++ public/js/mapbox.js
@@ -0,0 +1,47 @@
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamRvc3BpbiIsImEiOiJjanZvM2ZuejMwNnd6NGFvNHUzcTY3a2xuIn0.mHjASieo7rVyFD74p7QFDg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/jdospin/ckmc958pv4um017s31i5cnska',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // create new element (marker) to add to the map
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // add marker to the map
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // extend the map bounds to include the current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
